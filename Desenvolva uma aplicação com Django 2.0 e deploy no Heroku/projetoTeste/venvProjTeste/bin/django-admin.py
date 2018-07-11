07d45f69a15dbb5cdee31a917d17b0ddcb1e7cd0
Julio Kenji Ueda <julio.ueda@usp.br>
2018-07-11 16:50:50 -0300

    10.Models

---
+++ Desenvolva uma aplicação com Django 2.0 e deploy no Heroku/projetoTeste/venvProjTeste/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/akumaex/Documentos/Udemy/Desenvolva uma aplicação com Django 2.0 e deploy no Heroku/projetoTeste/venvProjTeste/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
