93f2cfd2c92e261df26ecc43f9cc9d9635921c0a
Julio Kenji Ueda <julio.ueda@usp.br>
2018-07-13 10:37:15 -0300

    15.Exibindo arquivos de media

--- Desenvolva uma aplicação com Django 2.0 e deploy no Heroku/projetoTeste/meuPrimeiroProjeto/meuPrimeiroProjeto/urls.py
+++ Desenvolva uma aplicação com Django 2.0 e deploy no Heroku/projetoTeste/meuPrimeiroProjeto/meuPrimeiroProjeto/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from .views import hello, articles, fname2
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('hello/', hello),
     path('articles/<int:year>/', articles),
     path('pessoa/<str:nome>/', fname2),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
