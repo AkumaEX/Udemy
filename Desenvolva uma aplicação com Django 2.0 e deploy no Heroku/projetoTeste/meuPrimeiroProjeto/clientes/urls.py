573d56f197b0ffd865047efecd586c52a53b21f0
Julio Kenji Ueda <julio.ueda@usp.br>
2018-07-16 14:53:12 -0300

    19.CRUD - Update (Atualizando cadastros existentes)

--- Desenvolva uma aplicação com Django 2.0 e deploy no Heroku/projetoTeste/meuPrimeiroProjeto/clientes/urls.py
+++ Desenvolva uma aplicação com Django 2.0 e deploy no Heroku/projetoTeste/meuPrimeiroProjeto/clientes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import persons_list, persons_new
+from .views import persons_list, persons_new, persons_update
 
 urlpatterns = [
     path('list/', persons_list, name='persons_list'),
     path('new/', persons_new, name='persons_new'),
+    path('update/<int:person_id>/', persons_update, name='persons_update'),
 ]
