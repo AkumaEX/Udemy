778be4b3cfa84e8d8295628391803bde168ad7fb
Julio Kenji Ueda <julio.ueda@usp.br>
2018-07-22 15:55:33 -0300

    33.Melhorando a estética do projeto com Bootstrap

--- Desenvolva uma aplicação com Django 2.0 e deploy no Heroku/projetoFinal/gestao_clientes/gestao_clientes/settings.py
+++ Desenvolva uma aplicação com Django 2.0 e deploy no Heroku/projetoFinal/gestao_clientes/gestao_clientes/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['udemy-django-gestao-clientes.herokuapp.com']
+ALLOWED_HOSTS = ['udemy-django-gestao-clientes.herokuapp.com', 'localhost']
 
 
 # Application definition
