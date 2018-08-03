fb1f82bf327f0676bc189228c81ce44f4ef5fa9a
Julio Kenji Ueda <julio.ueda@usp.br>
2018-08-03 15:57:56 -0300

    fim do curso

---
+++ Django 2.0 avançado, 100+ aulas domine framework like a boss/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/akumaex/Documentos/Udemy/Django 2.0 avançado, 100+ aulas domine framework like a boss/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
