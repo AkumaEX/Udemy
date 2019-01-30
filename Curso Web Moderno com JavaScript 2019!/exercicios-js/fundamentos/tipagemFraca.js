da5b308de716341e217a74f68a7b4a7c7189e272
JULIO KENJI UEDA <akumaex@gmail.com>
2019-01-30 14:01:22 -0200

    16. Tipos em JavaScript: Number

--- Curso Web Moderno com JavaScript 2019!/exercicios-js/fundamentos/tipagemFraca.js
+++ Curso Web Moderno com JavaScript 2019!/exercicios-js/fundamentos/tipagemFraca.js
@@ -1,13 +1,13 @@
-let qualquer = 'Legal'
-console.log(qualquer)
-console.log(typeof qualquer)
+let qualquer = 'Legal';
+console.log(qualquer);
+console.log(typeof qualquer);
 
-qualquer = 3.1415
-console.log(qualquer)
-console.log(typeof qualquer)
+qualquer = 3.1415;
+console.log(qualquer);
+console.log(typeof qualquer);
 
 // Evitar nomes genéricos
 
-let valor = ''
-let numero = 1
-let pqp = false // Produto Químico Perigoso... kkk
+let valor = '';
+let numero = 1;
+let pqp = false; // Produto Químico Perigoso... kkk
