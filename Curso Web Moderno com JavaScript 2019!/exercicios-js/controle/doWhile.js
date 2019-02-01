be4a032e9501246de116733676f1a129f779b8d3
JULIO KENJI UEDA <akumaex@gmail.com>
2019-02-01 15:54:10 -0200

    58. Usando a Estrutura DO/WHILE

--- Curso Web Moderno com JavaScript 2019!/exercicios-js/controle/doWhile.js
+++ Curso Web Moderno com JavaScript 2019!/exercicios-js/controle/doWhile.js
@@ -3,11 +3,11 @@ function getInteiroAleatorioEntre(min, max) {
     return Math.floor(valor);
 }
 
-let opcao = 0;
+let opcao = -1;
 
-while (opcao != -1) {
+do {
     opcao = getInteiroAleatorioEntre(-1, 10);
     console.log(`oção escolhida foi ${opcao}.`);
-}
+} while (opcao != -1);
 
 console.log('Até a próxima!');
